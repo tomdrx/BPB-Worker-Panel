71b89eb921535556d89a8ef64ec35e571386fac6
tomdrx <62591903+tomdrx@users.noreply.github.com>
2024-07-19 23:07:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '48597e69-84f3-4d93-8369-a556095e7632';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
@@ -200,7 +200,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
+                        url.hostname = 'cdn.33456789.xyz';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
